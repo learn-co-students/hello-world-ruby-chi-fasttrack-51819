3a43d5ff671ab538e7ed9b71dec666aea6d5b6c1
CYNTHIA BRAINOO <c.brainoo@ymail.com>
2019-05-18 17:26:49 +0000

    Done.

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -7,6 +7,5 @@ describe "Hello World" do
     # $stdout represents the output of your program, don't worry about how
     # we are testing this, focus on simply coding your solution.
     expect($stdout).to receive(:puts).with("Hello World!")
-    load './hello_world.rb'
-  end
-end
+    load './hello_world.
+// puts "hello_world!"
